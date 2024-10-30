75cabd15190775a3477e304e20edb0b5ef6cfdf2
Michal Kowal <michalkowal@Michals-MacBook-Pro.local>
2024-10-30 19:49:09 +0100

    Add color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
 	{
-		"catppucin/nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme catppucin]])
+			vim.cmd.colorscheme "catppuccin"
 		end,
 	},
 }
