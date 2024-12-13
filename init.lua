3dde42984f9bee63300e0fd3b5764a6c3f1ca828
michal kowal <michal.kowal@nordsec.com>
2024-12-13 08:42:01 +0100

    feat: update plugins, remap harpoon, add treewalker

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require("config.lazy")
 require("thegodenage")
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_set_keymap('n', '<C-j>', ':Treewalker Down<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':Treewalker Up<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-h>', ':Treewalker Left<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', ':Treewalker Right<CR>', { noremap = true })
